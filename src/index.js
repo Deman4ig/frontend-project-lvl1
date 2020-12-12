6761050b11fed4dd8c731fd59bad8bdaf2b40c7b
Dmitry Fomin <deman4ig@gmail.com>
2020-12-12 13:22:37 -0500

    fix magic number in game engine

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import getUserName from './cli.js';
 import printRules from './gameRules.js';
 
+const gameRounds = 3;
+
 export default (game) => {
   const name = getUserName();
   printRules(game.name);
-  for (let count = 0; count < 3; count += 1) {
+  for (let count = 0; count < gameRounds; count += 1) {
     const result = game();
     if (!result) {
       console.log(`Let's try again, ${name}!`);
